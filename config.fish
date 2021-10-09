76c4e1b2727184e275335e14d632bc47d7284bc1
Davysson Silva <mail@davysson.dev>
2021-10-09 12:09:29 -0300

    use pyenv instead of conda

--- config.fish
+++ config.fish
@@ -5,3 +5,7 @@ source ~/.asdf/asdf.fish
 # Wasmer
 export WASMER_DIR="/home/davysson/.wasmer"
 [ -s "$WASMER_DIR/wasmer.sh" ] && source "$WASMER_DIR/wasmer.sh"
+
+# pyenv
+status is-login; and pyenv init --path | source
+status is-interactive; and pyenv init - | source
