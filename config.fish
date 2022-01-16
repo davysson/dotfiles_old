edcd70fcabd10d82a9454f9d01fefa3732ece04d
Davysson Silva <mail@davysson.dev>
2022-01-16 17:44:25 -0300

    update dotfiles

--- config.fish
+++ config.fish
@@ -1,11 +1,2 @@
 # start starship prompt
 starship init fish | source
-# asdf
-source ~/.asdf/asdf.fish
-# Wasmer
-export WASMER_DIR="/home/davysson/.wasmer"
-[ -s "$WASMER_DIR/wasmer.sh" ] && source "$WASMER_DIR/wasmer.sh"
-
-# pyenv
-status is-login; and pyenv init --path | source
-status is-interactive; and pyenv init - | source
