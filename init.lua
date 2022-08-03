677ecd6af38c481c1035bd6583941e186e175ba7
Davysson Silva <mail@davysson.dev>
2022-08-02 23:24:17 -0300

    install basic plugins

--- init.lua
+++ init.lua
@@ -57,7 +57,6 @@ vim.opt.completeopt = {menu = true, menuone = true, preview = true, noselect = t
 
 ---------- Packages ----------
 
--- Packer
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
@@ -66,6 +65,31 @@ end
 return require('packer').startup(function(use)
   use { 'wbthomason/packer.nvim' }
   use { 'williamboman/mason.nvim', config = function() require("mason").setup() end }
+  use { 'windwp/nvim-autopairs', config = function() require("nvim-autopairs").setup {} end }
+  use { 'Pocco81/auto-save.nvim', config = function() require("auto-save").setup {execution_message = ""} end }
+  use { 'rmagatti/auto-session', config = function() require('auto-session').setup { log_level = 'error' } end }
+  --use { 'hrsh7th/nvim-cmp', config = function() require('cmp').setup{} end }
+  --use { 'RRethy/vim-illuminate' }
+  use { 'numToStr/Comment.nvim', config = function() require('Comment').setup() end }
+  use { 'kyazdani42/nvim-web-devicons', config = function() require("nvim-web-devicons").setup {default = true} end }
+  --use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim', config = function() require("diffview").setup() end }
+  --use { 'feline-nvim/feline.nvim', config = function () require('feline').setup() end }
+  use { 'lewis6991/gitsigns.nvim',  config = function() require('gitsigns').setup{current_line_blame = true} end }
+  use { 'phaazon/hop.nvim', config = function() require("hop").setup() end }
+  -- Luasnip
+  use { 'TimUntersberger/neogit', requires = { 'nvim-lua/plenary.nvim' }, config = function() require("neogit").setup{ 
+    signs = { section = { "→", "↓" }, item = { "→", "↓" }, hunk = { "", "" }}, 
+    integrations = { diffview = true }} 
+  end }
+  -- use { 'jose-elias-alvarez/null-ls.nvim', config = function() require("null-ls").setup() end }
+  use { 'sheerun/vim-polyglot' }
+  use { 'mhinz/vim-sayonara' }
+  -- Session Lens
+  -- Sidebar
+  use { 'nvim-telescope/telescope.nvim', config = function() require('telescope').setup{} end }
+  -- TODO
+  use {"akinsho/toggleterm.nvim", config = function() require("toggleterm").setup() end }
+  -- Treesitter
 
   if packer_bootstrap then
     require('packer').sync()
