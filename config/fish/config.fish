23af4fdd89bcd88f2f7df8d92275547b12a42962
Davysson Silva <mail@davysson.dev>
2022-08-05 23:57:22 -0300

    add node and deno to Containerfile

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,11 @@
 # remove fish greeting
 set fish_greeting
 
-# add rust executables to PATH
-export PATH="/opt/cargo/bin:$PATH"
-
 # set starship prompt
 starship init fish | source
 
 # add zoxide to shell
 zoxide init fish | source
+
+# add fnm to PATH
+fnm env --use-on-cd | source
