532b2d430023b8ff4db4bb1b2ad86ab6da0d9fa4
Davysson Silva <mail@davysson.dev>
2022-08-08 23:36:07 -0300

    update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,23 @@
 # remove fish greeting
 set fish_greeting
 
+# add rust to path
+export PATH="$HOME/.cargo/bin:$PATH"
+
+# add pyenv to path
+export PATH="$HOME/.pyenv/bin:$PATH"
+
+# add fnm to PATH
+fnm env --use-on-cd | source
+
+# set pyenv
+pyenv init - | source
+
+# add poetry to path
+export PATH="$HOME/.poetry/bin:$PATH"
+
 # set starship prompt
 starship init fish | source
 
 # add zoxide to shell
 zoxide init fish | source
-
-# add fnm to PATH
-fnm env --use-on-cd | source
