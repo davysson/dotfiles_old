f01edf981cc01c0b911b2999ad6583dafd49032c
Davysson Silva <mail@davysson.dev>
2022-09-03 21:59:26 -0300

    update config.fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,6 +4,9 @@ set fish_greeting
 # add rust to path
 export PATH="$HOME/.cargo/bin:$PATH"
 
+# add zls to path
+export PATH="$HOME/zls:$PATH"
+
 # add pyenv to path
 export PATH="$HOME/.pyenv/bin:$PATH"
 
@@ -16,8 +19,17 @@ pyenv init - | source
 # add poetry to path
 export PATH="$HOME/.poetry/bin:$PATH"
 
+# don`t create __pycache__ inside project
+export PYTHONPYCACHEPREFIX="$HOME/.cache/cpython/"
+
 # set starship prompt
 starship init fish | source
 
 # add zoxide to shell
 zoxide init fish | source
+
+# alias to run ML image
+set --local image "artigen"
+set --local directory "artigen"
+alias agrun="sudo docker run -it --network=host --device=/dev/kfd --device=/dev/dri --group-add=video --ipc=host --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $HOME/Projects/$directory:/$directory $image"
+alias agbuild="sudo docker build -t $image $HOME/Projects/$directory"
