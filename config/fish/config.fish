3e2c782a77d4549f2ecb8be63267211314f74803
Davysson Silva <mail@davysson.dev>
2023-01-26 14:40:25 -0300

    update fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,37 +1,58 @@
 # remove fish greeting
 set fish_greeting
 
-# add rust to path
-export PATH="$HOME/.cargo/bin:$PATH"
+# install fisher if it's not already installed
+if not functions -q fisher && test -z "$FISHER_INSTALL"
+    curl -sL https://git.io/fisher | source
+    FISHER_INSTALL=1 fisher install jorgebucaran/fisher jethrokuan/z jorgebucaran/autopair.fish jorgebucaran/replay.fish mattgreen/lucid.fish
+end
 
-# add zls to path
-export PATH="$HOME/zls:$PATH"
+# add dot/bin
+fish_add_path $HOME/.dot/bin
 
-# add pyenv to path
-export PATH="$HOME/.pyenv/bin:$PATH"
+# check if running in container
+if test -e /run/.containerenv
+    # prompt when running inside container
+    set -g lucid_prompt_symbol "#❯"
+    set -g lucid_prompt_symbol_error "#❯"
 
-# add ai/bin
-export PATH="$HOME/Projects/ai/bin:$PATH"
+    # update PATH
+    fish_add_path $CARGO_HOME/bin
+    fish_add_path $PYENV_ROOT/bin
 
-# add fnm to PATH
-fnm env --use-on-cd | source
+    # initialize pyenv
+    pyenv init - | source
+    pyenv virtualenv-init - | source
 
-# set pyenv
-pyenv init - | source
+    # load global virtualenv
+    pyenv activate global
 
-# add poetry to path
-export PATH="$HOME/.poetry/bin:$PATH"
+else
+    # prompt when running outside container
+    set -g lucid_prompt_symbol "❯"
+    set -g lucid_prompt_symbol_error "❯"
 
-# don`t create __pycache__ inside project
-export PYTHONPYCACHEPREFIX="$HOME/.cache/cpython/"
-
-# set starship prompt
-starship init fish | source
-
-# add zoxide to shell
-zoxide init fish | source
-
-# alias to run ML image
-set --local image "ai"
-set --local directory "ai"
-alias aibuild="sudo docker build -t $image $HOME/Projects/$directory"
+    function box
+        set -l NAME "dbx-ubuntu"
+        set -l IMAGE $NAME:22.04
+        set -l PYENV_ROOT "/opt/pyenv"
+    
+        # check if box already exists
+        if ! distrobox list | grep -q $NAME
+            distrobox create \
+                --additional-flags "\
+                    --device=/dev/kfd \
+                    --device=/dev/dri \
+                    --group-add keep-groups \
+                    --cap-add=SYS_PTRACE \
+                    --security-opt seccomp=unconfined" \
+                --yes \
+                --image $IMAGE \
+                --name $NAME \
+                --init-hooks "chown $USER:$USER -R /opt"
+        end
+    
+        # enter the created box
+        distrobox enter $NAME
+    end
+end
